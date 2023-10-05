092dd92ebb5363addf975b6d4c92ce284383abab
HZ <hongzhiee@gmail.com>
2023-10-05 17:57:44 +0800

    modification to about us

--- src/main/java/seedu/duke/Duke.java
+++ src/main/java/seedu/duke/Duke.java
@@ -7,14 +7,15 @@ public class Duke {
      * Main entry-point for the java.duke.Duke application.
      */
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
+        String logo = """
+                 ____        _       \s
+                |  _ \\ _   _| | _____\s
+                | | | | | | | |/ / _ \\
+                | |_| | |_| |   <  __/
+                |____/ \\__,_|_|\\_\\___|
+                """;
         System.out.println("Hello from\n" + logo);
         System.out.println("What is your name?");
-
         Scanner in = new Scanner(System.in);
         System.out.println("Hello " + in.nextLine());
     }
