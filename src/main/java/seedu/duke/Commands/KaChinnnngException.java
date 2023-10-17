42b7c8e4180f573fb8fbd206fb93f0615e025ede
Hongzhii <97496506+Hongzhii@users.noreply.github.com>
2023-10-17 16:10:18 +0800

    Revert "Add expense class"

--- src/main/java/seedu/duke/Commands/KaChinnnngException.java
+++ src/main/java/seedu/duke/Commands/KaChinnnngException.java
@@ -1,8 +1,5 @@
 package seedu.duke.commands;
 
-/**
- * Represents the exception thrown when there is an error in the command.
- */
 public class KaChinnnngException extends Exception{
     public KaChinnnngException(String message) {
         super(message);
